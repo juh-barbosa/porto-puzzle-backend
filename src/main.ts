1bf756b30b563dfec594f90dc3121f1bbbb84486
juh-barbosa <jujufreitas193@gmail.com>
2024-04-01 00:02:23 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -7,8 +7,13 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors();
+  app.enableCors({
+    origin: 'http://localhost:3000',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
   await app.listen(process.env.APPLICATION_PORT || 5000);
+
 }
 
 bootstrap();
