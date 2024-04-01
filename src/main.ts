2db96472f59c6634513df8c279cf2c778324110c
juh-barbosa <jujufreitas193@gmail.com>
2024-03-31 21:59:19 -0300

    update

--- src/main.ts
+++ src/main.ts
@@ -2,18 +2,31 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import * as dotenv from 'dotenv';
 
-async function bootstrap() {
-  dotenv.config();
+dotenv.config();
 
+async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.enableCors({
-    origin: 'http://localhost:3000',
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
+  // Adicione o middleware de CORS personalizado
+  app.use((req, res, next) => {
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    res.setHeader('Access-Control-Allow-Origin', '*');
+    res.setHeader(
+      'Access-Control-Allow-Methods',
+      'GET,OPTIONS,PATCH,DELETE,POST,PUT',
+    );
+    res.setHeader(
+      'Access-Control-Allow-Headers',
+      'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+    );
+    if (req.method === 'OPTIONS') {
+      res.status(200).end();
+      return;
+    }
+    next();
   });
-  await app.listen(process.env.APPLICATION_PORT || 5000);
 
+  await app.listen(process.env.APPLICATION_PORT || 5000);
 }
 
 bootstrap();
